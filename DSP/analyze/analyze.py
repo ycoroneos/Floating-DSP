4bddd34ac40bb58aec5c90490b84024cab16e844
Yanni Coroneos <spock@csail.mit.edu>
2017-05-13 19:01:24 -0400

    generic fft function that can do arbitrary bin sizes

--- DSP/analyze/analyze.py
+++ DSP/analyze/analyze.py
@@ -19,39 +19,22 @@ def sine(fs, f):
     return t, np.sin(2.0*np.pi*f*t)
 
 
-def getFFT(fs, x, freqlo, freqhi):
-    xf=scipy.fftpack.fft(x)
-    freq=np.fft.fftfreq(len(x), d=1.0/fs)
-    return (2.0/len(x))* np.abs(xf)[freqlo:freqhi]
 
-def showFFT(fs, x, freqlo, freqhi):
-    xf=scipy.fftpack.fft(x, fs*1000)
-    freq=np.fft.fftfreq(len(x), d=1.0/fs)
-    #plt.plot(freq[freqlo:freqhi],(2.0/len(x))* np.abs(xf)[freqlo:freqhi])
-    plt.semilogx(freq[freqlo:freqhi],(2.0/len(x))* np.abs(xf)[freqlo:freqhi])
-    plt.show()
-
-def showFFTdB(fs, x, freqlo, freqhi):
-    xf=scipy.fftpack.fft(x, 1024)
-    freq=np.fft.fftfreq(len(x), d=1.0/fs)
-    #plt.plot(freq[freqlo:freqhi],(2.0/len(x))* np.abs(xf)[freqlo:freqhi])
-    #plt.semilogx(freq[freqlo:freqhi],(2.0/len(x))* np.abs(xf)[freqlo:freqhi])
-    dat=20*np.log10(xf/max(xf))
-    plt.semilogx(freq[freqlo:freqhi], dat)
-    plt.show()
-
-def fir_freqz(b, fs):
-    N=1024*10
-    # Get the frequency response
+def showFFT(b, fs=48000, bins=1024*10, logy=False, justplot=False):
+    N=bins
     X = np.fft.fft(b, N)
-    # Take the magnitude
-    Xm = np.abs(X)
-    # Convert the magnitude to decibel scale
-    Xdb = 20*np.log10(Xm/Xm.max())
-    # Frequency vector
-    f = np.arange(N)*fs/N
-    plt.semilogx(f, Xdb, 'b', label='Orig. coeff.')
-    plt.show()
+    if logy:
+        Xm=np.abs(X)
+        Xm = 20*np.log10(Xm/Xm.max())
+    else:
+        Xm = np.abs(X) * (2.0/len(X))
+    t=np.fft.fftfreq(len(Xm), d=1.0/fs)
+    if justplot:
+        plt.semilogx(t, Xm)
+        plt.show()
+    else:
+        return t, Xm
+
 
 def readfloatfile(filename):
     return map(float, [line.strip() for line in open(filename, 'r')])
@@ -65,21 +48,8 @@ def calcTHDN(fftsig, fundamental):
 
 
 if (__name__=="__main__"):
-    #generate a sample signal
     sig=readfloatfile("coeff_fixed_int.list")
     #sig=readfloatfile("chirp.signal")
-    #w,h = scipy.signal.freqz(sig)
-    #h_dB = 20 * np.log10(abs(h))
-    #plt.plot(w/max(w), h_dB)
-    #plt.show()
-    fir_freqz(sig, 48000)
-    #showFFTdB(48000*100, sig, 0, 48000)
-    #t, sq=square(48000, 2, 4)
-   # t, sq=sine(48000, 2)
-    #fft=getFFT(48000, sq, 0, 100)
-    #print calcTHDN(fft, 2)
-#    plt.plot(t, sq)
-#    plt.show()
-#    t2, sn2 = sine(48000, 50)
-#    t3, sn3 = sine(48000, 20)
-#    showFFT(48000, sq, 0, 100)
+    #t, sq=sine(48000, 2000)
+    #t, sq=square(48000, 2000, 4)
+    showFFT(sig, 48000, justplot=True, logy=True)
