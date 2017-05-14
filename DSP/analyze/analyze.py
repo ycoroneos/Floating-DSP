8a684181bb4cc9d5067bde450549acd2515e1361
Yanni Coroneos <spock@csail.mit.edu>
2017-05-13 20:27:08 -0400

    function to calculate THD+N of a signal

--- DSP/analyze/analyze.py
+++ DSP/analyze/analyze.py
@@ -20,7 +20,7 @@ def sine(fs, f):
 
 
 
-def showFFT(b, fs=48000, bins=1024*10, logy=False, justplot=False):
+def showFFT(b, fs=48000, bins=1024*50, logy=False, justplot=False):
     N=bins
     X = np.fft.fft(b, N)
     if logy:
@@ -39,17 +39,38 @@ def showFFT(b, fs=48000, bins=1024*10, logy=False, justplot=False):
 def readfloatfile(filename):
     return map(float, [line.strip() for line in open(filename, 'r')])
 
-def calcTHDN(fftsig, fundamental):
-    denom = fftsig[fundamental]/np.sqrt(2)
-    numerator = sum(map(lambda x: np.power((x/np.sqrt(2)),2.0), fftsig))
-    numerator -= np.power(denom, 2.0)
-    numerator = np.sqrt(numerator)
-    return numerator / denom
+#def calcTHDN(fftsig, fundamental):
+#    denom = fftsig[fundamental]/np.sqrt(2)
+#    numerator = sum(map(lambda x: np.power((x/np.sqrt(2)),2.0), fftsig))
+#    numerator -= np.power(denom, 2.0)
+#    numerator = np.sqrt(numerator)
+#    return numerator / denom
 
+
+def calcTHDN(fftscale, fftsig, fundamental, bounds=20.0):
+    #helper
+    rmsfunc = lambda v:np.sqrt(np.sum(map(lambda x: x*x, v)))
+    #first zero in on the fundamental frequency
+    scalefact=fftscale[1]
+    fund_lo = fundamental-bounds
+    fund_hi = fundamental+bounds
+    index_lo = np.floor(fund_lo/scalefact)
+    index_hi = np.floor(fund_hi/scalefact)
+    index_end = np.floor(24000.0/scalefact)
+    denominator = rmsfunc(fftsig[index_lo:index_hi])
+    numerator = rmsfunc(fftsig[index_hi+1:index_end])
+    return numerator / denominator
+
+#calculates THD+N for a some given signal samples
+def signalTHDN(signal, fs=48000, fuzz=20.0):
+    freqscale, value = showFFT(signal, fs, justplot=False, logy=False)
+    return calcTHDN(freqscale, value, 100, bounds=fuzz)
 
 if (__name__=="__main__"):
-    sig=readfloatfile("coeff_fixed_int.list")
+    #sig=readfloatfile("coeff_fixed_int.list")
     #sig=readfloatfile("chirp.signal")
     #t, sq=sine(48000, 2000)
-    #t, sq=square(48000, 2000, 4)
-    showFFT(sig, 48000, justplot=True, logy=True)
+    print signalTHDN(square(48000, 100, 4)[1])
+#    t, sq=square(48000, 100, 4)
+#    freqscale, value = showFFT(sq, 48000, justplot=False, logy=False)
+#    print calcTHDN(freqscale, value, 100, bounds=20.0)
